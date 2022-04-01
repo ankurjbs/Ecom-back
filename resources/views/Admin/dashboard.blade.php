78294fc8b737d8fd871b8ce5874db80ca52afd5f
Ankur Maurya <ankurjbs@gmail.com>
2022-04-01 12:30:25 +0530

    basic routes and templates ready

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.appAdmin')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="text-black-50">Welcome !</h1>
+    </div>
+@endsection
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+    <a href="{{ route('dashboard') }}" class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
-        <p>Home</p>
+        <p>Menu 1</p>
     </a>
 </li>
